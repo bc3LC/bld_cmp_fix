e330d636e3ca6a25311ca46d16b63eeeaaaba6dc
Jon Sampedro <jon.sampedro@bc3.net>
2024-09-24 17:32:43 +0200

    add new function proposed by YZ

--- fit_trad_function.R
+++ fit_trad_function.R
@@ -23,26 +23,24 @@ serv_coal <- read.csv("input/serv_coal.csv") %>%
   # Transform to GJ
   mutate(en_GJ = en_EJ * 1E9,
          en_GJ_flsp = en_GJ / flsp) %>%
-  filter(en_EJ > 1E-4,
-         price != 0)
+  filter(price != 0)
 
 
 serv_TradBio <- read.csv("input/serv_TradBio.csv") %>%
   filter(year >= 1975) %>%
   as_tibble() %>%
   left_join_error_no_match(flsp, by = join_by(GCAM_region_ID, year)) %>%
-  # Transform to GJ
-  mutate(en_GJ = en_EJ * 1E9,
-         en_GJ_flsp = en_GJ / flsp) %>%
-  filter(en_GJ_flsp != 0)
+  mutate(en_EJ_flsp = en_EJ / flsp) %>%
+  filter(en_EJ_flsp != 0,
+         price != 0)
   
 #--------------
 # 1- TRADITIONAL BIOMASS
 # Fit function for traditional biomass:
 # Given its carbon neutrality and that the price is exogenous (not linked with the land module, we can drop the price effect out)
 
-formula.tradBio<- "en_GJ_flsp ~ x / (pcgdp_thous)"
-start.value.tradBio <- c(x = 1)
+formula.tradBio<- "en_EJ_flsp ~ a * pcgdp_thous^-b * price^-c"
+start.value.tradBio <- c(a = 1, b = 1, c = 1)
 
 # Use minipack.lm for the estimation (need to check the prediction)
 fit_tradBio = minpack.lm::nlsLM(formula.tradBio, serv_TradBio, start = start.value.tradBio)
@@ -55,7 +53,7 @@ fit_tradBio_data <- fit_tradBio$m
 
 Residuals_tradBio <- fit_tradBio_data$resid()
 SumResSquared_tradBio <- sum(Residuals_tradBio^2)
-TotalSumSquares_tradBio <- sum((serv_TradBio$en_GJ_flsp - mean(serv_TradBio$en_GJ_flsp))^2)
+TotalSumSquares_tradBio <- sum((serv_TradBio$en_EJ_flsp - mean(serv_TradBio$en_EJ_flsp))^2)
 RSquared_tradBio <- 1 - (SumResSquared_tradBio / TotalSumSquares_tradBio)
 RSquared_tradBio
 
@@ -81,11 +79,11 @@ ggsave("figures/Fit_TradBio.tiff", last_plot(), "tiff")
 # Fit function for coal:
 # The function should make the demand decrease as income and/or price increase (negative price and income effects)
 
-formula.coal<- "en_GJ_flsp  ~ (A / pcgdp_thous) + (k / price)"
+formula.coal<- "en_GJ_flsp ~ a * pcgdp_thous^-b * price^-c"
 
 
 # Manual starting values
-start.value.coal <- c(A = 1, k = 0)
+start.value.coal <- c(a = 1, b = 1, c = 1)
 
 # Use minipack.lm for the estimation (need to check the prediction)
 fit_coal <- minpack.lm::nlsLM(formula.coal, serv_coal, start = start.value.coal)
@@ -96,11 +94,11 @@ summary(fit_coal)
 # Check R2
 fit_coal_data <- fit_coal$m
 
-# Residuals_coal <- fit_coal_data$resid()
-# SumResSquared_coal <- sum(Residuals_coal^2)
-# TotalSumSquares_coal <- sum((serv_coal$en_GJ_flsp - mean(serv_coal$en_GJ_flsp))^2)
-# RSquared_coal <- 1 - (SumResSquared_coal / TotalSumSquares_coal)
-# RSquared_coal
+Residuals_coal <- fit_coal_data$resid()
+SumResSquared_coal <- sum(Residuals_coal^2)
+TotalSumSquares_coal <- sum((serv_coal$en_GJ_flsp - mean(serv_coal$en_GJ_flsp))^2)
+RSquared_coal <- 1 - (SumResSquared_coal / TotalSumSquares_coal)
+RSquared_coal
 
 A_coal<-coef(fit_coal)[1]
 k_coal<-coef(fit_coal)[2]
