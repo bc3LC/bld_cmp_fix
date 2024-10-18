7f0f780de5420778556e8d47ac2adff7d81523b2
Jon Sampedro <jon.sampedro@bc3.net>
2024-10-18 14:16:38 +0200

    add new estimations

--- fit_trad_function.R
+++ fit_trad_function.R
@@ -21,9 +21,19 @@ serv_coal <- read.csv("input/serv_coal.csv") %>%
   as_tibble() %>%
   left_join_error_no_match(flsp, by = join_by(GCAM_region_ID, year)) %>%
   # Transform to GJ
-  mutate(en_GJ = en_EJ * 1E9,
-         en_GJ_flsp = en_GJ / flsp) %>%
-  filter(price != 0)
+  mutate(en_EJ_flsp = en_EJ / flsp) %>%
+  mutate(log_en_EJ_flsp = log(en_EJ_flsp),
+         log_pcgdp_thous = log(pcgdp_thous),
+         log_price = log(price),
+         log_sq_pcgdp_thous = log_pcgdp_thous^2,
+         GCAM_region_ID = as.character(GCAM_region_ID)) %>%
+  filter(en_EJ_flsp != 0,
+         price != 0)%>%
+  filter(complete.cases(.))
+
+coal_heat <- serv_coal %>% filter(grepl("heating", service))
+coal_oth <- serv_coal %>% filter(grepl("other", service))
+
 
 
 serv_TradBio <- read.csv("input/serv_TradBio.csv") %>%
@@ -31,93 +41,116 @@ serv_TradBio <- read.csv("input/serv_TradBio.csv") %>%
   as_tibble() %>%
   left_join_error_no_match(flsp, by = join_by(GCAM_region_ID, year)) %>%
   mutate(en_EJ_flsp = en_EJ / flsp) %>%
+  mutate(log_en_EJ_flsp = log(en_EJ_flsp),
+         log_pcgdp_thous = log(pcgdp_thous),
+         log_price = log(price),
+         log_sq_pcgdp_thous = log_pcgdp_thous^2,
+         GCAM_region_ID = as.character(GCAM_region_ID)) %>%
   filter(en_EJ_flsp != 0,
-         price != 0)
+         price != 0) %>%
+  filter(complete.cases(.))
+
+tradBio_heat <- serv_TradBio %>% filter(grepl("heating", service))
+tradBio_oth <- serv_TradBio %>% filter(grepl("other", service))
   
 #--------------
 # 1- TRADITIONAL BIOMASS
 # Fit function for traditional biomass:
 # Given its carbon neutrality and that the price is exogenous (not linked with the land module, we can drop the price effect out)
+#formula.tradBio<- "en_EJ_flsp ~ a * pcgdp_thous^-b * price^-c"
+#formula.tradBio<- "log(en_EJ_flsp) ~ a + log(pcgdp_thous) + log(pcgdp_thous^2) +log(price) + GCAM_region_ID"
 
-formula.tradBio<- "en_EJ_flsp ~ a * pcgdp_thous^-b * price^-c"
-start.value.tradBio <- c(a = 1, b = 1, c = 1)
+prelast_tradBio <- -0.4
 
 # Use minipack.lm for the estimation (need to check the prediction)
-fit_tradBio = minpack.lm::nlsLM(formula.tradBio, serv_TradBio, start = start.value.tradBio)
+fit_tradBio_heat = lm(log_en_EJ_flsp ~ log_pcgdp_thous + log_sq_pcgdp_thous + GCAM_region_ID,
+                        data = tradBio_heat)
+
+fit_tradBio_oth = lm(log_en_EJ_flsp ~ log_pcgdp_thous + log_sq_pcgdp_thous + GCAM_region_ID,
+                      data = tradBio_oth)
 
 # Check coefficients
-summary(fit_tradBio)
+summary(fit_tradBio_heat)
+summary(fit_tradBio_oth)
 
-# Check R2
-fit_tradBio_data <- fit_tradBio$m
+# Predict
+data_tradBio <- serv_TradBio %>%
+  mutate(b1_heat = as.numeric(fit_tradBio_heat$coefficients[1]),
+         b2_heat = as.numeric(fit_tradBio_heat$coefficients[2]),
+         b3_heat = as.numeric(fit_tradBio_heat$coefficients[3]),
+         b1_oth = as.numeric(fit_tradBio_oth$coefficients[1]),
+         b2_oth = as.numeric(fit_tradBio_oth$coefficients[2]),
+         b3_oth = as.numeric(fit_tradBio_oth$coefficients[3]),
+         prelast = prelast_tradBio) %>%
+  mutate(pred_log_en_EJ_flsp = if_else(grepl("heat", service), 
+                                       b1_heat + b2_heat * log_pcgdp_thous + b3_heat * log_sq_pcgdp_thous + prelast_tradBio * log_price,
+                                       b1_oth + b2_oth * log_pcgdp_thous + b3_oth * log_sq_pcgdp_thous + prelast_tradBio * log_price))
 
-Residuals_tradBio <- fit_tradBio_data$resid()
-SumResSquared_tradBio <- sum(Residuals_tradBio^2)
-TotalSumSquares_tradBio <- sum((serv_TradBio$en_EJ_flsp - mean(serv_TradBio$en_EJ_flsp))^2)
-RSquared_tradBio <- 1 - (SumResSquared_tradBio / TotalSumSquares_tradBio)
-RSquared_tradBio
+data_tradBio_adder <- data_tradBio %>%
+  group_by(GCAM_region_ID) %>%
+  filter(year == max(year)) %>%
+  ungroup() %>%
+  mutate(en_EJ_flsp = exp(log_en_EJ_flsp),
+         pred_en_EJ_flsp = exp(pred_log_en_EJ_flsp)) %>%
+  mutate(bias.adder = en_EJ_flsp - pred_en_EJ_flsp) %>%
+  select(GCAM_region_ID, fuel, service, bias.adder)
 
-# Predicted df
-predicted_serv_TradBio <- serv_TradBio %>%
-  mutate(x = coef(fit_tradBio),
-         en_GJ_flsp_pred = x / pcgdp_thous,
-         en_EJ_pred = en_GJ_flsp_pred * flsp / 1E9)
-
-# Plot
-ggplot(serv_TradBio, aes(x = pcgdp_thous, y = en_EJ)) + 
-  geom_point() +
-  theme_bw() +
-  labs(x = "Per capita GDP (thous$/pers)", y = ("GJ_flsp")) + 
-  geom_point(color='red', data = predicted_serv_TradBio, aes(x = pcgdp_thous, y = en_EJ_pred), pch = 4) + 
-  ggtitle("Fitted function for Traditional Biomass demand in the residential sector",
-          subtitle = "Service_GJ_flsp ~ x / pcgdp_thous")
-
-ggsave("figures/Fit_TradBio.tiff", last_plot(), "tiff")
+data_tradBio_fin <- data_tradBio %>%
+  left_join(data_tradBio_adder, join_by(GCAM_region_ID, fuel, service)) %>%
+  mutate(pred_en_EJ_flsp = exp(pred_log_en_EJ_flsp)) %>%
+  mutate(pred_en_EJ_flsp_adj = pred_en_EJ_flsp + bias.adder) %>%
+  select(GCAM_region_ID, fuel, service, year, en_EJ_flsp, pred_en_EJ_flsp = pred_en_EJ_flsp_adj)
 
 #--------------
 # 2- COAL
 # Fit function for coal:
 # The function should make the demand decrease as income and/or price increase (negative price and income effects)
-
-formula.coal<- "en_GJ_flsp ~ a * pcgdp_thous^-b * price^-c"
-
-
-# Manual starting values
-start.value.coal <- c(a = 1, b = 1, c = 1)
+#formula.coal<- "en_GJ_flsp ~ a * pcgdp_thous^-b * price^-c"
+#start.value.coal <- c(a = 1, b = 1, c = 1)
 
 # Use minipack.lm for the estimation (need to check the prediction)
-fit_coal <- minpack.lm::nlsLM(formula.coal, serv_coal, start = start.value.coal)
+fit_coal_heat <- lm(log_en_EJ_flsp ~ log_pcgdp_thous  + GCAM_region_ID,
+               data = coal_heat)
+
+fit_coal_oth <- lm(log_en_EJ_flsp ~ log_pcgdp_thous  + GCAM_region_ID,
+                    data = coal_oth)
 
 # Check coefficients
-summary(fit_coal)
+summary(fit_coal_heat)
+summary(fit_coal_oth)
 
-# Check R2
-fit_coal_data <- fit_coal$m
+# Predict
+prelast_coal <- -0.5
 
-Residuals_coal <- fit_coal_data$resid()
-SumResSquared_coal <- sum(Residuals_coal^2)
-TotalSumSquares_coal <- sum((serv_coal$en_GJ_flsp - mean(serv_coal$en_GJ_flsp))^2)
-RSquared_coal <- 1 - (SumResSquared_coal / TotalSumSquares_coal)
-RSquared_coal
+data_coal<- serv_coal %>%
+  mutate(b1_heat = as.numeric(fit_coal_heat$coefficients[1]),
+         b2_heat = as.numeric(fit_coal_heat$coefficients[2]),
+         b3_heat = 0, 
+         b1_oth = as.numeric(fit_coal_oth$coefficients[1]),
+         b2_oth = as.numeric(fit_coal_oth$coefficients[2]),
+         b3_oth = 0, 
+         prelast = prelast_coal) %>%
+  mutate(pred_log_en_EJ_flsp = if_else(grepl("heat", service), 
+                                       b1_heat + b2_heat * log_pcgdp_thous + b3_heat * log_sq_pcgdp_thous + prelast_tradBio * log_price,
+                                       b1_oth + b2_oth * log_pcgdp_thous + b3_oth * log_sq_pcgdp_thous + prelast_tradBio * log_price))
 
-A_coal<-coef(fit_coal)[1]
-k_coal<-coef(fit_coal)[2]
+data_coal_adder <- data_coal %>%
+  group_by(GCAM_region_ID) %>%
+  filter(year == max(year)) %>%
+  ungroup() %>%
+  mutate(en_EJ_flsp = exp(log_en_EJ_flsp),
+         pred_en_EJ_flsp = exp(pred_log_en_EJ_flsp)) %>%
+  mutate(bias.adder = en_EJ_flsp - pred_en_EJ_flsp) %>%
+  select(GCAM_region_ID, fuel, service, bias.adder)
 
-# Predicted df
-predicted_serv_coal <- serv_coal %>%
-  mutate(A = A_coal,
-         k = k_coal,
-         en_GJ_flsp_pred = (A / pcgdp_thous) + (k /price),
-         en_EJ_pred = en_GJ_flsp_pred * flsp / 1E9)
+data_coal_fin <- data_coal %>%
+  left_join(data_coal_adder, join_by(GCAM_region_ID, fuel, service)) %>%
+  mutate(pred_en_EJ_flsp = exp(pred_log_en_EJ_flsp)) %>%
+  mutate(pred_en_EJ_flsp_adj = pred_en_EJ_flsp + bias.adder) %>%
+  select(GCAM_region_ID, fuel, service, year, en_EJ_flsp, pred_en_EJ_flsp = pred_en_EJ_flsp_adj)
 
-# Plot
-ggplot(serv_coal, aes(x = pcgdp_thous, y = en_EJ)) + 
-  geom_point() +
-  theme_bw() +
-  labs(x = "Per capita GDP (thous$/pers)", y = ("GJ_flsp")) + 
-  geom_point(color='red', data = predicted_serv_coal, aes(x = pcgdp_thous, y = en_EJ_pred), pch = 4) + 
-  ggtitle("Fitted function for Coal demand in the residential sector",
-          subtitle = "Service_GJ_flsp ~ x / pcgdp_thous")
 
-ggsave("figures/Fit_Coal_GDP.tiff", last_plot(), "tiff")
+
+
+
 
